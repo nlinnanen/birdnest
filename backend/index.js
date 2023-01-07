ecb3948642658f6ef8fda662e9e8807f5d3ccccc
Niklas Linnanen <nlinnanen@gmail.com>
2023-01-07 15:33:31 +0200

    Fix linting, refactor, and document backend

--- backend/index.js
+++ backend/index.js
@@ -18,6 +18,9 @@ const distance = (x, y) => Math.sqrt((x - ORIGIN.x) ** 2 + (y - ORIGIN.y) ** 2)
 const pilots = new Map()
 const timeouts = new Map()
 
+const updatePilots = (socket) =>
+  socket.emit('pilots', Array.from(pilots.values()))
+
 setInterval(() => {
   console.log('Fetching drones...')
   axios
@@ -28,10 +31,14 @@ setInterval(() => {
           const serial = drone.serialNumber[0]
           const dist = distance(drone.positionX[0], drone.positionY[0])
 
+          // Check if drone is in NDZ
+          // if so fetch pilot data if not already fetched
+          // and update minimum distance and last violation time to map
+          // and update timout so that the pilot info is deleted after 10 minutes
           if (dist < RADIUS) {
-            console.log('Drone in NDZ: ', serial)
-            if (pilots.has(serial)) {
+            if (pilots.has(serial)) { // pilot data is already fetched
               clearTimeout(timeouts.get(serial))
+              // update minimum distance and last violation time
               const prev = pilots.get(serial)
               pilots.set(serial, {
                 ...prev,
@@ -39,34 +46,32 @@ setInterval(() => {
                 lastViolation: new Date(),
               })
             } else {
+              // fetch pilot data
               axios
                 .get(
                   `https://assignments.reaktor.com/birdnest/pilots/${serial}`
                 )
                 .then((pilotRes) => {
-                  console.log(
-                    `Pilot for drone ${serial} fetched: `,
-                    pilotRes.data
-                  )
                   pilots.set(serial, {
                     pilot: pilotRes.data,
                     minimumDistance: dist,
                     lastViolation: new Date(),
                   })
-                  io.emit('pilots', Array.from(pilots.values()))
+                  // emit pilots to all sockets
+                  updatePilots(io)
                 })
                 .catch((pilotErr) => {
                   console.error("Couldn't fetch pilot: ", pilotErr)
                 })
             }
 
+            // set timeout to delete pilot data after 10 minutes
             timeouts.set(
               serial,
               setTimeout(() => {
-                console.log('deleting ', serial)
                 pilots.delete(serial)
                 timeouts.delete(serial)
-                io.emit('pilots', Array.from(pilots.values()))
+                updatePilots(io)
               }, 1000 * 60 * 10)
             )
           }
@@ -80,7 +85,7 @@ setInterval(() => {
 
 io.on('connection', (socket) => {
   console.log('socket', socket.id, ' connected')
-  socket.emit('pilots', Array.from(pilots.values()))
+  updatePilots(socket)
 })
 
 io.listen(8080)
